097bd4f152541b42926c11ad15f879a9a21bbf14
JoeChildress <joe.ch@comcast.net>
2024-10-21 16:10:30 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Joe's Azure Dev Sandbox</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
